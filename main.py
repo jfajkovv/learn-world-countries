d1bd5ac7968acbe76c0388205c0b656f263fe360
Julian Fajkowski <jfajkovv@gmail.com>
2022-06-24 17:33:10 +0200

    fetch system screen resolution

---
+++ main.py
@@ -0,0 +1,9 @@
+# Modules import section.
+import tkinter
+
+root = tkinter.Tk()
+
+SYS_SCREEN_W = root.winfo_screenwidth()
+SYS_SCREEN_H = root.winfo_screenheight()
+
+print(f"{SYS_SCREEN_W}x{SYS_SCREEN_H}")
