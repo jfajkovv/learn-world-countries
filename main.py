c20450d30c6dd61c6bf90b0a49c597a4630fb2be
Julian Fajkowski <jfajkovv@gmail.com>
2022-07-01 17:21:57 +0200

    change function output in tk_helpers.py (get_screen_geometry) so that it fits tk's window geometry setup; lay app skeleton in main.py

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 # Modules import section.
-import tk_helpers
+import tkinter as tk
+import tk_helpers as tk_h
 
-ACTIVE_SCREEN_SIZE = tk_helpers.get_screen_geometry()
-print(ACTIVE_SCREEN_SIZE)
+# Handy global settings.
+
+# This one fetches active screen resolution and returns it as a list.
+# First indice is width, second -- height.
+ACTIVE_SCREEN_SIZE = tk_h.get_screen_geometry()
+#print(ACTIVE_SCREEN_SIZE)
+#WINDOW_WIDTH = ACTIVE_SCREEN_SIZE[0]
+#WINDOW_HEIGHT = ACTIVE_SCREEN_SIZE[1]
+
+# https://www.freepik.com/premium-vector/world-map-with-countries-borders-outline_20366547.htm
+WORLD_MAP_IMG = "world-map-with-countries-borders-outline.png"
+
+
+class MainAppGUI(tk.Frame):
+    """General GUI aggregator class."""
+
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry(ACTIVE_SCREEN_SIZE)
+    MainAppGUI(parent=root).pack()
+    root.mainloop()
--- tk_helpers.py
+++ tk_helpers.py
@@ -1,11 +1,12 @@
 def get_screen_geometry():
-    """Returns active screen size as a list."""
+    """Returns active screen resolution as a list."""
     import tkinter
     root = tkinter.Tk()
     root.update_idletasks()
     root.attributes("-fullscreen", True)
     root.state("iconic")
     screen_geometry = root.winfo_geometry()
-    screen_geometry = screen_geometry.split('+', 1)[0].split('x')
+#    screen_geometry = screen_geometry.split('+', 1)[0].split('x')
+    screen_geometry = screen_geometry.split('+', 1)[0]
     root.destroy()
     return screen_geometry
