b239b5f85d7fdec470e4bf00bd95f3dff40c2509
Julian Fajkowski <jfajkovv@gmail.com>
2022-07-03 13:27:15 +0200

    create Controls class and use it inside MainAppGUI()

--- main.py
+++ main.py
@@ -15,16 +15,31 @@ ACTIVE_SCREEN_SIZE = tk_h.get_screen_geometry()
 WORLD_MAP_IMG = "world-map-with-countries-borders-outline.png"
 
 
+class Controls(tk.Frame):
+    """Simple control pane."""
+
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+
+        self.button = tk.Button(master, text="this")
+        self.button.pack()
+        self.button = tk.Button(master, text="that")
+        self.button.pack()
+
 class MainAppGUI(tk.Frame):
     """General GUI aggregator class."""
 
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+    def __init__(self, master):
+        super().__init__()
+        self.master = master
+        master.title("Learn World Countries")
+
+        self.control_pane = Controls(master=self.master)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry(ACTIVE_SCREEN_SIZE)
-    MainAppGUI(parent=root).pack()
+    MainAppGUI(master=root).pack()
     root.mainloop()
