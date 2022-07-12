ba0604a8daa8b4b317a4f1e96580d5d3e78375f4
Julian Fajkowski <jfajkovv@gmail.com>
2022-07-12 14:24:37 +0200

    set up basic gameplay behaviour

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ PIN_INIT_COORDS = (145.0, 231.0)
 coords_data = pd.read_csv(COORDS_DATA_FILE)  # Prepare data file via pandas.
 all_countries = coords_data.country.to_list()
 known_countries = []
+unknown_countries = []
+next_country = INIT_COUNTRY
+next_country_coords = PIN_INIT_COORDS
+known = 0
+unknown = 0
 
 
 def set_pin(pin, coords):
@@ -99,21 +104,56 @@ def fetch_country_coords(country_name, data):
     return (float(drawn_country_xcoor), float(drawn_country_ycoor))
 
 
-def get_user_input(init_pos=True):
+def update_known_lbl():
+    global known
+    known += 1
+    next_lbl = f"Known: {known}"
+    known_lbl.config(text=next_lbl)
+
+
+def update_unknown_lbl():
+    global unknown
+    unknown += 1
+    next_lbl = f"Unknown: {unknown}"
+    unknown_lbl.config(text=next_lbl)
+
+
+def get_user_input():
     """Fetches player country answer."""
     user_input = answer_entry.get().title()
     clear_entry_field()
 
-    if init_pos:
-        if check_answer(answer=user_input, correct=INIT_COUNTRY):
-            mark_good(coords=PIN_INIT_COORDS)
-        else:
-            mark_wrong(coords=PIN_INIT_COORDS)
-        init_pos = False
+    global next_country
+    global next_country_coords
+    global known
+    global unknown
 
-def main():
-    answer_entry.config(state="normal")
+    if user_input == next_country:
+        mark_good(coords=next_country_coords)
+        known_countries.append(next_country)
+        update_known_lbl()
+    else:
+        if next_country not in unknown_countries:
+            mark_wrong(coords=next_country_coords)
+            unknown_countries.append(next_country)
+            update_unknown_lbl()
+
+    if not all_countries:
+        answer_entry.config(state="disabled")
+        answer_bttn.config(state="disabled")
+        t_pin.hideturtle()
+        print("info: countries list depleted.")
+    else:
+        next_country = draw_country(states_list=all_countries, learned=known_countries)
+        all_countries.remove(next_country)
+        next_country_coords = fetch_country_coords(country_name=next_country, data=coords_data)
+        set_pin(pin=t_pin, coords=next_country_coords)
+
+
+def start():
     init_pin(turtle=t_pin)
+    answer_entry.config(state="normal")
+    answer_bttn.config(state="normal")
 
 
 # Tkinter master application instance.
@@ -129,7 +169,7 @@ top_bar_frm.pack(fill=tk.BOTH, side=tk.TOP)
 controls_frm = tk.Frame(master=top_bar_frm)
 controls_frm.pack(fill=tk.BOTH, side=tk.LEFT)
 
-start_bttn = tk.Button(master=controls_frm, text="Start", command=main)
+start_bttn = tk.Button(master=controls_frm, text="Start", command=start)
 start_bttn.pack(side=tk.LEFT)
 
 fscreen_bttn = tk.Button(
@@ -160,6 +200,14 @@ root.bind("<Return>", (lambda event: get_user_input()))
 
 answer_bttn = tk.Button(master=input_frm, text="Answer", command=get_user_input)
 answer_bttn.pack(side=tk.LEFT, padx=10)
+answer_bttn.config(state="disabled")
+
+known_lbl = tk.Label(master=input_frm, text=f"Known: {known}")
+known_lbl.pack(side=tk.LEFT, padx=10)
+
+unknown_lbl = tk.Label(master=input_frm, text=f"Unknown: {unknown}")
+unknown_lbl.pack(side=tk.LEFT, padx=10)
+
 
 # Exit section.
 exit_frm = tk.Frame(master=top_bar_frm)
