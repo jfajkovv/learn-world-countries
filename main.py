b522a4143d6681258c2641a66cf409883f39c7f0
Julian Fajkowski <jfajkovv@gmail.com>
2022-07-12 16:15:52 +0200

    create TlPool class and evoke it from MainApplication's constructor; import turtle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Load all necessary modules.
 import tkinter as tk
 import tk_helpers as tk_h
+import turtle as tl
 
 # Handy global constants.
 APP_TITLE = "Learn World Countries"
@@ -76,6 +77,13 @@ class UserInput(tk.Frame):
         ).pack(side=tk.LEFT, padx=10)
 
 
+class TlPool(tl.ScrolledCanvas):
+    """Handy scrolled background container."""
+
+    def __init__(self, master):
+        super().__init__(master)
+
+
 class MainApplication(tk.Frame):
     """Application core structure"""
 
@@ -87,12 +95,14 @@ class MainApplication(tk.Frame):
         self.controls = Controls(master=self.top_bar)
         self.separator = SeparatorFrame(master=self.top_bar)
         self.input = UserInput(master=self.top_bar)
+        self.canvas = TlPool(master=root)
 
         # Place GUI components onto main frame.
-        self.top_bar.pack(fill=tk.BOTH, side=tk.TOP)
+        self.top_bar.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
         self.controls.pack(fill=tk.BOTH, side=tk.LEFT)
         self.separator.pack(expand=True, side=tk.LEFT)
         self.input.pack(expand=True, side=tk.LEFT)
+        self.canvas.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
 
 
 if __name__ == "__main__":
@@ -101,5 +111,5 @@ if __name__ == "__main__":
     root.title(APP_TITLE)
     root.geometry(ROOT_WINDOW)
 #    root.attributes("-fullscreen", True)
-    MainApplication(master=root).pack(side="top", fill="both", expand=True)
+    MainApplication(master=root).pack(side=tk.TOP, expand=True, fill=tk.BOTH)
     root.mainloop()
