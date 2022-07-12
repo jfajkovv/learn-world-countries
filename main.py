935a5aed28d211b213368759d11e414f4a201c28
Julian Fajkowski <jfajkovv@gmail.com>
2022-07-12 15:09:18 +0200

    create TopBar class and evoke it from the MainApplication's constructor

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import tkinter as tk
 
 # Handy global constants.
 APP_TITLE = "Learn World Countries"
-ROOT_WINDOW = "1280x720"  # Reduce window to 720p after minimising.
+ROOT_WINDOW = "1280x720"  # reduce window to 720p after minimising
+
+
+class TopBar(tk.Frame):
+    """Top bar controls aggregator."""
+
+    def __init__(self, master):
+        super().__init__()
 
 
 class MainApplication(tk.Frame):
@@ -12,8 +19,15 @@ class MainApplication(tk.Frame):
     def __init__(self, master):
         super().__init__()
 
+        # Construct GUI components.
+        self.top_bar = TopBar(master=self)
+
+        # Place GUI components onto main frame.
+        self.top_bar.pack(fill=tk.BOTH, side=tk.TOP)
+
 
 if __name__ == "__main__":
+    # Tkinter root application instance.
     root = tk.Tk()
     root.title(APP_TITLE)
     root.geometry(ROOT_WINDOW)
